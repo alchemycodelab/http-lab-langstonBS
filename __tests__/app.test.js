244c740370b505bd3cd6b27e3d034f0bd54bd786
langstonBS <langston.beckwithstanley@gmail.com>
2020-06-08 15:33:00 -0700

    got green to pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -18,5 +18,24 @@ describe('app routes', () => {
         });
     });
   });
+    
+  describe('app routes', () => {
+    it('handles the blue route', () => {
+      return request(app)
+        .get('/blue')
+        .then(res => {
+          expect(res.text).toEqual('<h1>hi blue</h1>');
+        });
+    });
+  });
+  describe('app routes', () => {
+    it('handles the green route', () => {
+      return request(app)
+        .get('/green')
+        .then(res => {
+          expect(res.text).toEqual('<h1>hi green</h1>');
+        });
+    });
+  });
 });
 
