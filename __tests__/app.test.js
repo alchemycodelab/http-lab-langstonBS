0c13d18eec1ef036db0e5a8b423bcb978fd582bd
langstonBS <langston.beckwithstanley@gmail.com>
2020-06-08 15:26:30 -0700

    first test in appjs to pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,12 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('createResponse', () => {
-
+describe('app routes', () => {
+  it('handles the / route', () => {
+    return request(app)
+      .get('/')
+      .then(res => {
+        expect(res.text).toEqual('<h1>hi</h1>');
+      });
+  });
 });
